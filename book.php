89e3118339527f0c4830f7088f450d9c1e433501
bibekpandeya <cricketbibek619@gmail.com>
2023-06-12 17:19:24 +0545

    first commit

--- book.php
+++ book.php
@@ -35,8 +35,75 @@
 </section>
 <!-- header section end-->
 <div class="heading" style="background:url(images/header-bg-3.jpg) no-repeat">
-    <h1>book </h1>
+    <h1>book now </h1>
 </div>
+
+ 
+<!-- booking section starts -->
+<section class="booking">
+<h1 class="heading-title">book your trip!</h1>
+<form action ="book_form.php" mehtot ="post" class="book-form">
+
+<div class="flex">
+<div class="inputBox">
+    <span>name :</span>
+    <input type="text" placeholder="enter your name" name="name">
+</div>
+
+
+<div class="inputBox">
+    <span>email :</span>
+    <input type="text" placeholder="enter your email" name="email">
+</div>
+
+<div class="inputBox">
+    <span>phone :</span>
+    <input type="number" placeholder="enter your number" name="phone">
+</div>
+
+<div class="inputBox">
+    <span>address :</span>
+    <input type="text" placeholder="enter your address" name="address">
+</div>
+
+<div class="inputBox">
+    <span>where to visit:</span>
+    <input type="text" placeholder="place you want to visit " name="location">
+</div>
+
+<div class="inputBox">
+    <span>how many :</span>
+    <input type="number" placeholder="number of guests" name="guests">
+</div>
+
+<div class="inputBox">
+    <span>arrivals :</span>
+    <input type="date"  name="arrivals ">
+</div>
+
+<div class="inputBox">
+    <span>leaving:</span>
+    <input type="date " name="leaving">
+</div>
+
+</div>
+<input type="submit" value="submit" class="btn" name="send">
+</form>
+
+
+</section>
+
+
+<!-- booking section ends-->
+
+
+
+
+
+
+
+
+
 
 
 <!-- footer section starts-->
